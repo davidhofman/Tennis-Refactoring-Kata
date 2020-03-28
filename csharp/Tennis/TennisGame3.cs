95813ec391032625007fca92402e26ba10b9a971
David Hofman <davidhofman1996@gmail.com>
2020-03-28 17:52:28 +0100

    broke down GetScore into more methods

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -17,25 +17,27 @@ namespace Tennis
 
         public string GetScore()
         {
-            string scoreName;
-            if ((p2Score < 4 && p1Score < 4) && (p2Score + p1Score < 6))
-            {
-                string[] scoreNames = { "Love", "Fifteen", "Thirty", "Forty" };
-                scoreName = scoreNames[p1Score];
-                return (p2Score == p1Score) ? scoreName + "-All" : scoreName + "-" + scoreNames[p2Score];
-            }
-            else
-            {
-                if (p2Score == p1Score) return "Deuce";
-                string leadingPlayer = p1Score > p2Score ? p1Name : p2Name;
-                return (Math.Abs(p1Score - p2Score) == 1) ? "Advantage " + leadingPlayer : "Win for " + leadingPlayer;
-            }
+            return ((p2Score < 4 && p1Score < 4) && (p2Score + p1Score < 6)) ? GetScoreFirstPhase() : GetScoreSecondPhase();
         }
 
         public void WonPoint(string playerName)
         {
             if (playerName == p1Name) p1Score ++;
             else p2Score ++;
+        }
+
+        private string GetScoreFirstPhase()
+        {
+            string[] scoreNames = { "Love", "Fifteen", "Thirty", "Forty" };
+            string p1ScoreName = scoreNames[p1Score];
+            return (p2Score == p1Score) ? p1ScoreName + "-All" : p1ScoreName + "-" + scoreNames[p2Score];
+        }
+
+        private string GetScoreSecondPhase()
+        {
+            if (p2Score == p1Score) return "Deuce";
+            string leadingPlayer = p1Score > p2Score ? p1Name : p2Name;
+            return (Math.Abs(p1Score - p2Score) == 1) ? "Advantage " + leadingPlayer : "Win for " + leadingPlayer;
         }
 
     }
